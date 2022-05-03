49e703f9a1b43791b09f4af744e600b07a506327
Glitch (jl-giphy) <none>
2022-05-03 22:21:33 +0000

    ☕🦖 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
 $(".search-button").click(function() {
+$(".gifImgHolder").empty();
 let userQuery = $(".search-term").val();
 let apiURL = `https://api.giphy.com/v1/gifs/search?q=${userQuery}&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1`;
 
@@ -8,13 +9,28 @@ let apiURL = `https://api.giphy.com/v1/gifs/search?q=${userQuery}&rating=pg&api_
     return response.json();
   })
   .then(function(json) {
-    let randomInd = Math.floor(Math.random() * 50);
-    // let imgURL = json
-    let gifURL = json.data[randomInd].images.original.url;
-    $(".gifImg").attr("src",gifURL);
-    $(".gifImg").css("display","block");
+    let numGifs = $(".numItems").val();
+    if (numGifs != null || numGifs != "") {
+        for (let i = numGifs; i > 0; i--) {
+          generateGIF(json);
+        }
+    }
+    else {
+      generateGIF(json);
+    }
     console.log(json);
   })
   
 
 });
+
+function generateGIF(json) {
+  let randomInd = Math.floor(Math.random() * 50);
+    // let imgURL = json
+    // let gifImgObj = document.createElement("img");
+    // gifImgObj.attr("class","gifImg");
+    let gifURL = json.data[randomInd].images.original.url;
+    // gifImgObj.attr("src",gifURL);
+    // gifImgObj.css("display","block");
+$(".gifImgHolder").append($(`<img src = ${gifURL} class = "gifImg">`));
+}
